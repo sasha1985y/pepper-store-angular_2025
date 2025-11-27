6410a937235a04c7f173896198d15296d7f67dde
Igor Alexeenko <alexeenko.igor@gmail.com>
2025-11-27 21:28:22 +0300

    Add filters markdown

--- project/src/app/app.ts
+++ project/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { AppHeader } from './app-header/app-header';
 import { AppFooter } from './app-footer/app-footer';
+import { Checkbox } from './checkbox/checkbox';
 
 @Component({
   selector: 'app-root',
-  imports: [/*RouterOutlet, */AppHeader, AppFooter],
+  imports: [/*RouterOutlet, */AppHeader, AppFooter, Checkbox],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
